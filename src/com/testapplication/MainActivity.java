80eeaa641d1b410b5cf7b6d51afe4b8e9489aa99
Jonathan Stowell <jons@illycorp.com>
2012-07-26 15:45:47 +0100

    Initial Commit.

---
+++ src/com/testapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.testapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
